6607f4259cf2a655d0da2a9a874e59b6f9888d69
CoderRedwing <ajiteshru@gmail.com>
2025-10-10 23:58:39 +0530

    python task

---
+++ python/day1/basics.py
@@ -0,0 +1,16 @@
+# variables 
+"""
+so variable is name that store the data in memory
+
+like 
+
+name = "Aman"
+age= 23
+
+here name and age are variable 
+"""
+
+name = "Ajitesh"
+age = 23
+
+print(name, " ", age)
